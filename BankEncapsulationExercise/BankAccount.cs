2a43ac35bb4e5223ad4fbc8e42f1099eada7a81c
Nick Stockton <phoenix76728@gmail.com>
2021-08-30 12:18:30 -0400

    initial commit

---
+++ BankEncapsulationExercise/BankAccount.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace BankEncapsulationExercise
+{
+    class BankAccount
+    {
+        private double balance = 0;
+        
+        public void Deposite (double amount)
+        {
+            balance += amount;
+        }
+        public double GetBalance()
+        {
+            return balance;
+        }
+
+    }
+}
---
+++ BankEncapsulationExercise/Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+
+namespace BankEncapsulationExercise
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            var bank1 = new BankAccount();
+            Console.WriteLine("How much would you like to deposite?");
+            int use = Convert.ToInt32(Console.ReadLine());
+            bank1.Deposite(use);
+
+            Console.WriteLine("Would you like to see your balance?   (yes or no)");
+            string use2 = Console.ReadLine();
+
+            if (use2 == "yes")
+            {
+               Console.WriteLine( "You have a balance of: $" +bank1.GetBalance());
+            }
+            else
+            {
+                Console.WriteLine("Have a great day!");
+            }
+
+
+        }
+    }
+}
